500a4f0c2a0edb8703f412d865e7557c3eab3380
EleanaCano <eleanacano1@gmail.com>
2023-08-04 14:50:04 -0600

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,36 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
+
   onLoaded(img: string){
     console.log('log padre', img);
   }
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.userService.create({
+      email: "eleana@mail.com",
+      password: "changeme2023",
+      name: "Eleana",
+      role: "customer",
+      avatar: "https://picsum.photos/640/640?r=7816"
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
+
+  login() {
+    this.authService.login('eleana@mail.com', 'changeme2023')
+    .subscribe(rta => {
+      console.log(rta);
+    });
   }
 }
