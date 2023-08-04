e06570b75c5d9dad550201aa3d606657ef0db5e8
Eleana Cano <eleanacano1@gmail.com>
2023-08-04 16:32:52 -0600

    tokenInterceptor-v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { ImgComponent } from './components/img/img.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductsVComponent } from './components/products-v/products-v.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +25,15 @@ import { NavComponent } from './components/nav/nav.component';
     ImgComponent,
     ProductsComponent,
     ProductsVComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CategoryComponent,
+    MyCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 import { Auth } from './../models/auth.model';
 import { User } from './../models/user.model';
+import { switchMap, tap } from 'rxjs';
+import { R3TargetBinder } from '@angular/compiler';
+import { TokenService } from './../services/token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +14,32 @@ export class AuthService {
   private apiUrl = 'https://api.escuelajs.co/api/v1/auth'
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService
   ) { }
 
   login(email:string, password:string) {
-    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password});
+    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password})
+    .pipe(
+      tap(response => this.tokenService.saveToken(response.access_token))
+    );
   }
 
-  profile(token: string) {
+  profile() {
     //const headers = new HttpHeaders();
     //headers.set('Authorization', `Bearer ${token}`);
     return this.http.get<User>(`${this.apiUrl}/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        //'Content-type': 'application/json'
-      }
+      // headers: {
+      //   Authorization: `Bearer ${token}`,
+      //   //'Content-type': 'application/json'
+      //}
     });
+  }
+
+  loginAndGet(email: string, password: string) {
+    return this.login(email, password)
+    .pipe(
+      switchMap(rta => this.profile(rta.access_token))
+    )
   }
 }
