ba325398f3281577a472a907b1a61daba76ef1f6
Eleana Cano <eleanacano1@gmail.com>
2023-08-09 10:06:34 -0600

    exitGuard

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { OnExit } from '../../guards/exit.guard';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnExit{
+
+  onExit() {
+    const rta = confirm('Are you sure you want to leave?');
+    return rta;
+  };
 
 }
